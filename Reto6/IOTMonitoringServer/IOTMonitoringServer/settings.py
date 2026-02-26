2b04a26d741c0d8407c5ee3ee86a07f618d49086
Angel Henao <ay.henao@uniandes.edu.co>
2026-02-25 20:09:41 -0500

    update db instance

--- Reto6/IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ Reto6/IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -95,7 +95,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "100.31.122.177",  # Dirección IP de la base de datos
+        "HOST": "100.53.34.23",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
