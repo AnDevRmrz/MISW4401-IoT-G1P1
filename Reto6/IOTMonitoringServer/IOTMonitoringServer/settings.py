1cb50135461b756d2ab69398347af4232cf530ae
Angel Henao <ay.henao@uniandes.edu.co>
2026-03-03 18:32:59 -0500

    update public ips

--- Reto6/IOTMonitoringServer/IOTMonitoringServer/settings.py
+++ Reto6/IOTMonitoringServer/IOTMonitoringServer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-bu+)8ft@9+qd*#e#f_s@wkyv2tmq+#!a^3j15h3kjk^jzksu0j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "10.0.35.4", "54.173.41.112"]
+ALLOWED_HOSTS = ["localhost", "10.0.35.4", "3.221.155.45"]
 
 
 # Application definition
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": "iot_data",  # Nombre de la base de datos
         "USER": "dbadmin",  # Nombre de usuario
         "PASSWORD": "uniandesIOT1234*",  # Contraseña
-        "HOST": "100.53.34.23",  # Dirección IP de la base de datos
+        "HOST": "44.199.199.164",  # Dirección IP de la base de datos
         "PORT": "",  # Puerto de la base de datos
     }
 }
@@ -160,7 +160,7 @@ LOGOUT_REDIRECT_URL = '/login/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Dirección del bróker MQTT
-MQTT_HOST = "100.31.237.136"
+MQTT_HOST = "44.214.92.128"
 
 # Puerto del bróker MQTT
 MQTT_PORT = 8082
